929405e8ad6eb3575f3294066d2597e19b4af88c
Mohd Shahid <sidmirza4@gmail.com>
2022-06-20 23:16:48 +0530

    update schema

--- main-service/src/utility/schemas.ts
+++ main-service/src/utility/schemas.ts
@@ -6,12 +6,12 @@ export const promptSchema = yup.object({
 });
 
 export const resolutionSchema = yup.object({
-  height: yup.number().min(50).max(500).required(),
-  width: yup.number().min(50).max(500).required(),
+  height: yup.number().min(50).required(),
+  width: yup.number().min(50).required(),
 });
 
 export const generateRequestSchema = yup.object({
   prompts: yup.array(promptSchema).min(1).required(),
   resolution: resolutionSchema.required(),
-  cycles: yup.number().min(0).max(100).required(),
+  cycles: yup.number().min(200).max(400).required(),
 });
